f4f223d63506c7de836b7d2f305d7229da30b597
Bhavani <trbhavanilearn@gmail.com>
2026-06-29 10:21:23 +0530

    uv and day11 learnings

---
+++ day10/app12.py
@@ -0,0 +1,13 @@
+from pypdf import PdfReader
+
+pdf_path = "Sachin.pdf"
+
+reader = PdfReader(pdf_path)
+print("Metadata:", reader.metadata)
+
+text = ""
+
+for i, page in enumerate(reader.pages):
+    text += page.extract_text() + "\n"
+
+print(text)
